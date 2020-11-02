69a9414d372bdcb2de4c6db6168f1d344bbce2cb
Ned Hulseman <ned.hulseman@ey.com>
2020-11-02 07:31:47 -0500

    Recipe table fully functional

--- app.py
+++ app.py
@@ -19,31 +19,33 @@ def main():
 @app.route('/search-recipes')
 def recipe_search():
     recipe_df = pd.read_excel('data/recipe_list.xlsx', sheet_name='Cookbook')
-    '''
-    recipe_df = pd.DataFrame({
-        'Name': ['Alfreds Futterkiste', 'Ian An', 'Al Maddock'],
-        'Country': ['Germany', 'Sweden', 'UK']
-    })
-    '''
-    soup = BeautifulSoup(recipe_df.to_html(index=False), "html.parser")
+    recipe_df = recipe_df.fillna("")
+    recipe_df['keywords'] = recipe_df['food_name'].astype(str) +recipe_df['season'].astype(str)  +recipe_df['food_type'].astype(str)+recipe_df['crockpot'].astype(str) +recipe_df['source'].astype(str)
+    recipe_df['img_source'] = recipe_df['img_source'].apply(lambda x: '<img class="food-img" src={}></img>'.format(x))
+
+    keep_cols = ['keywords','img_source', 'food_name', 'source']
+    soup = BeautifulSoup(recipe_df[keep_cols].to_html(index=False, escape=False), "html.parser")
     soup.find('table')['id'] = 'recipe-table'
-    soup.find('tr')['class'] = 'header'
+    rows = soup.find_all('tr')
+    rows[0]['class'] = 'header'
+
+    rows[0].find_all('th')[0]['class'] = 'keyword-column'
+    for i in range(recipe_df.shape[0]):#enumerate(recipe_df['meal_id']):
+        rows[i+1]['id'] = recipe_df['meal_id'].iloc[i]
+        rows[i+1].find_all('td')[0]['class'] = 'keyword-column'
+        image = soup.new_tag('td')
+
+    #new_tag = soup.new_tag("Your Tag") #add even more tags as needed
+    #new_tag.append("Your Text")
+    # insert the new tag after the last tag using insert_after
+    #the_last_tag_i_want_to_insert_after.insert_after(new_tag)
+
 
     dets = "Here are ingredients... Here are instructions..."
 
     return render_template("recipe-search.html", table=soup, dets=dets)
 
-@app.route('/get-ins')
-def get_ins():
-    return 'Here are ingredients...\nHere are instructions...'
 
-@app.route('/boarding-sim')
-def boarding_sim():
-    return render_template("boarding-sim.html")
-
-@app.route('/xgboost-allstate')
-def xgboost_allstate():
-    return render_template("xgboost-allstate.html")
 
 
 if __name__ == "__main__":
